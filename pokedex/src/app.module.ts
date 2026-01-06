929dcf013a2dbc38f16050d6dcff938f2649031e
facundo moya <facundomoya18@hotmail.com>
2026-01-06 11:39:27 -0300

    add Joi validation for environment configuration

--- pokedex/src/app.module.ts
+++ pokedex/src/app.module.ts
@@ -7,14 +7,16 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { envConfig } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
 
     ConfigModule.forRoot({
-      load: [envConfig]
+      load: [envConfig],
+      validationSchema: JoiValidationSchema
     }),
-    
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
--- pokedex/src/config/env.config.ts
+++ pokedex/src/config/env.config.ts
@@ -2,4 +2,5 @@ export const envConfig = () => ({
     environment: process.env.NODE_ENV || 'development',
     mongodb: process.env.MONGODB,
     port: process.env.PORT,
+    defaultLimit: process.env.DEFAULT_LIMIT || 7,
 });
