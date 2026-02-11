a498a3f40bd9c0ff0467fcafa1a97d1fc9b5b2af
facundo moya <facundomoya18@hotmail.com>
2026-02-11 10:31:04 -0300

    refactor ProductsService and ProductsController: implement NotFoundException handling and improve UUID parsing

--- teslo-shop/src/products/products.service.ts
+++ teslo-shop/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,36 +8,40 @@ import { handleDBException } from 'src/utils/handleDbExceptions';
 
 @Injectable()
 export class ProductsService {
-
-  private readonly logger = new Logger('ProductsService');
   constructor(
     @InjectRepository(Product)
-    private readonly productoRepository: Repository<Product>,
+    private readonly productRepository: Repository<Product>,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try{
-      const product = this.productoRepository.create(createProductDto);
-      await this.productoRepository.save(product);
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
       return product;
     }catch(error){
      handleDBException(error);
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    const products = await this.productRepository.find();
+    return products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({id});
+    if(!product) throw new NotFoundException(`Product with id ${id} not found`);
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    if(!product) throw new NotFoundException(`Product with id ${id} not found`);
+    await this.productRepository.remove(product);
+    return { message: `Product with id ${id} has been removed` };
   }
 }
