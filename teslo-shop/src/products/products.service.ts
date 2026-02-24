2da38d473f9e458b5fa6d11aa87096c75c87e9aa
facundo moya <facundomoya18@hotmail.com>
2026-02-24 10:55:59 -0300

    refactor ProductsService: enhance findOne method to use query builder for flexible searching by title or slug

--- teslo-shop/src/products/products.service.ts
+++ teslo-shop/src/products/products.service.ts
@@ -40,11 +40,16 @@ export class ProductsService {
     if(isUUID(term)){
       product = await this.productRepository.findOneBy({id: term});
     }else {
-      product = await this.productRepository.findOneBy({slug: term});
+      //product = await this.productRepository.findOneBy({slug: term});
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder.where(`title =:title or slug =:slug`, {
+        title: term.toUpperCase(),
+        slug: term.toLowerCase(),
+      }).getOne();
     }
     
     //const product = await this.productRepository.findOneBy({id});
-    if(!product) throw new NotFoundException(`Product with id ${term} not found`);
+    //if(!product) throw new NotFoundException(`Product with id ${term} not found`);
     return product;
   }
 
