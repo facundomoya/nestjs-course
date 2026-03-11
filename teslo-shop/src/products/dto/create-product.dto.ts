593b1576d2c465d939b8d7fd2a99e0e0507658d9
facundo moya <facundomoya18@hotmail.com>
2026-03-11 11:45:16 -0300

    add images property to CreateProductDto and update ProductsService to handle image creation

--- teslo-shop/src/products/dto/create-product.dto.ts
+++ teslo-shop/src/products/dto/create-product.dto.ts
@@ -32,6 +32,11 @@ export class CreateProductDto {
     @IsOptional()
     tags: string[];
 
+    @IsString({ each: true })
+    @IsArray()
+    @IsOptional()
+    images?: string[];
+
     @IsIn(['men', 'women', 'kid', 'unisex'])
     gender: string;
 }
