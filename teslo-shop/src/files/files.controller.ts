7e4cc548c06eb9ff4d2260b249c4ccbd3fbcd1be
facundo moya <facundomoya18@hotmail.com>
2026-05-23 12:59:05 -0300

    feat: add fileNamer helper for dynamic file naming during uploads

--- teslo-shop/src/files/files.controller.ts
+++ teslo-shop/src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
@@ -11,6 +12,7 @@ export class FilesController {
   @Post('product')
   @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter, storage: diskStorage({
     destination: './static/uploads',
+    filename: fileNamer
   }) }))
   uploadFile(@UploadedFile() file: Express.Multer.File){
     if(!file) {
