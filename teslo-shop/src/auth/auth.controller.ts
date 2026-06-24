ba089d0403872d6ea8a0d474a58d91392be1adbb
facundo moya <facundomoya18@hotmail.com>
2026-06-23 23:40:00 -0300

    feat: implement user login functionality with bcrypt password hashing and validation

--- teslo-shop/src/auth/auth.controller.ts
+++ teslo-shop/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,11 @@ export class AuthController {
   @Post('/register')
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
+  }
+
+  @Post('/login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.loginUser(loginUserDto);
   }
 
 }
--- teslo-shop/src/auth/auth.service.ts
+++ teslo-shop/src/auth/auth.service.ts
@@ -1,24 +1,48 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm'
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async create(createUserDto: CreateUserDto) {
     try {
-     const user = this.userRepository.create(createUserDto);
-     await this.userRepository.save(user);
-     return user;
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: await bcrypt.hash(password, 10)
+      });
+      await this.userRepository.save(user);
+      return user;
     } catch (error) {
       this.hadleDBExceptions(error);
     }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+      const { email, password } = loginUserDto;
+      const user = await this.userRepository.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+
+      const { password: _, ...userNoPassword } = user;
+      return userNoPassword;
   }
 
   private hadleDBExceptions(error: any) {
