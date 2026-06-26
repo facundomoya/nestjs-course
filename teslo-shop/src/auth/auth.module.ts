3cdfb85c63efef4a7b5f8da1c4a7dff6993b923d
facundo moya <facundomoya18@hotmail.com>
2026-06-25 23:34:08 -0300

    feat: add JWT authentication with Passport and JWT modules, implement JwtStrategy and JwtPayload interface

--- teslo-shop/src/auth/auth.module.ts
+++ teslo-shop/src/auth/auth.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PassportModule } from '@nestjs/passport/dist/passport.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+  TypeOrmModule.forFeature([User]),
+  PassportModule.register({ defaultStrategy: 'jwt' }),
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => {
+      return {
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '2h'
+        }
+      }
+    }
+  })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule]
 })
-export class AuthModule {}
+export class AuthModule { }
---
+++ teslo-shop/src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,4 @@
+export interface JwtPayload {
+    email: string;
+    //everything that we want to store in the token, we can add it here
+}
