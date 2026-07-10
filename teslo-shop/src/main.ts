663a86b98b16310a4b7ad6fe755739fac12297b6
facundo moya <facundomoya18@hotmail.com>
2026-07-10 13:08:36 -0300

    feat: integrate Swagger for API documentation and update dependencies

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { ErrorLoggingInterceptor } from './common/interceptors/logger.interceptor';
 import { Logger } from '@nestjs/common/services/logger.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
-  
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
@@ -16,7 +17,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
-app.useGlobalInterceptors(new ErrorLoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFull API')
+    .setDescription('Teslo shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalInterceptors(new ErrorLoggingInterceptor());
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Application is running on port ${process.env.PORT ?? 3000}`);
 }
